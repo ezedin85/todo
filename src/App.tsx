d8fa8c5d3b8774e93b9abf198fdc67b6765f40ee
Ezedin <ezedinnuru85@gmail.com>
2024-01-30 11:57:30 +0300

    branch updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { useLocalStorage } from './hooks/useLocalStorage'
 
 
 function App() {
-console.log("this")
+console.log("this is branch 3")
     const { getItem } = useLocalStorage('todos')
     const [todos, setTodos] = useState<Todo[]>([])
     const [action, setAction] = useState<Action>({ type: 'add', id: 0 })
